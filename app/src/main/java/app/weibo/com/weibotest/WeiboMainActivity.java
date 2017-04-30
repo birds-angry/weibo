1b93108a71d964d92d012e579ebe46435972fa86
shuai <614004869@qq.com>
2017-04-30 11:58:47 +0800

    first commit

---
+++ app/src/main/java/app/weibo/com/weibotest/WeiboMainActivity.java
@@ -0,0 +1,13 @@
+package app.weibo.com.weibotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeiboMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weibo_main);
+    }
+}
